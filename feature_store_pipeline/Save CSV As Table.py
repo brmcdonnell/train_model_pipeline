a64de85654d7cb357ee11bfcdaa09c587b3c4348
Sean Mason <mason.s@gmail.com>
2024-10-08 09:27:27 -0500

    Update Save CSV As Table.py

--- feature_store_pipeline/Save CSV As Table.py
+++ feature_store_pipeline/Save CSV As Table.py
@@ -2,7 +2,7 @@
 # Settings to connect to Azure storage account blob storage
 spark.conf.set("fs.azure.account.auth.type.edsdbpocdata.dfs.core.windows.net", "SAS")
 spark.conf.set("fs.azure.sas.token.provider.type.edsdbpocdata.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.edsdbpocdata.dfs.core.windows.net", "sv=2022-11-02&ss=bfqt&srt=sco&sp=rltfx&se=2023-09-23T02:44:56Z&st=2023-06-22T18:44:56Z&spr=https&sig=nHF4guDKSdTTy2VcLtMjpRKyk6dBkoh9rRDf9DwZM24%3D")
+spark.conf.set("fs.azure.sas.fixed.token.edsdbpocdata.dfs.core.windows.net", "sv=2022-11-02&ss=bfqt&srt=co&sp=rwdlacupiytfx&se=2025-12-05T23:20:35Z&st=2024-10-08T14:20:35Z&spr=https&sig=u%2F3obQPLORBrQlfwRpsDdR20b24eu2CbfFncvZllVEw%3D")
 
 # COMMAND ----------
 
